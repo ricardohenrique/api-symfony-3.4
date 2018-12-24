29b79bad1aa6955fc1a8af1ad35c9f545b1118f7
ricardohenrique <ricardo.hb@outlook.com>
2018-12-24 13:51:51 -0300

    serialize return

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -19,6 +20,8 @@ class ProductController extends Controller
     public function indexAction()
     {
         $products = $this->getDoctrine()->getRepository('AppBundle:Product')->findAll();
-        return new JsonResponse($products);
+        $products = $this->get('jms_serializer')->serialize($products, 'json');
+        return new Response($products);
+//        return new JsonResponse($products);
     }
 }
