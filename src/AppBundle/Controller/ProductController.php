37b33bdd3c7dd31419b1627b804ce0423c753ae7
ricardohenrique <ricardo.hb@outlook.com>
2018-12-24 13:57:10 -0300

    get product

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Product;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,14 +15,25 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ProductController extends Controller
 {
-
     /**
      * @Route("/")
+     * @Method("GET")
      */
     public function indexAction()
     {
         $products = $this->getDoctrine()->getRepository('AppBundle:Product')->findAll();
         $products = $this->get('jms_serializer')->serialize($products, 'json');
+        return new Response($products);
+//        return new JsonResponse($products);
+    }
+
+    /**
+     * @Route("/{id}")
+     * @Method("GET")
+     */
+    public function getAction(Product $product)
+    {
+        $products = $this->get('jms_serializer')->serialize($product, 'json');
         return new Response($products);
 //        return new JsonResponse($products);
     }
